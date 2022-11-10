3730a830fa9096faf7b0d110f924a67b38e4e02e
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2022-11-10 23:25:34 +0200

    unit-test HttpServiceNotfoundPipe

--- src/repos/pipes/http-service-notfound.pipe.spec.ts
+++ src/repos/pipes/http-service-notfound.pipe.spec.ts
@@ -1,7 +1,32 @@
+import { NotFoundException } from '@nestjs/common';
 import { HttpServiceNotfoundPipe } from './http-service-notfound.pipe';
 
 describe('HttpServiceNotfoundPipe', () => {
+  let value;
   it('should be defined', () => {
     expect(new HttpServiceNotfoundPipe()).toBeDefined();
+  });
+
+  it('should throw NotFoundexception', () => {
+    value = {
+      response: {
+        status: 404,
+      },
+    };
+    const httpServiceNotfoundPipe: HttpServiceNotfoundPipe =
+      new HttpServiceNotfoundPipe();
+    expect(() => httpServiceNotfoundPipe.transform(value)).toThrowError(
+      new NotFoundException(),
+    );
+  });
+  it('should return value', () => {
+    value = {
+      response: {
+        status: 200,
+      },
+    };
+    const httpServiceNotfoundPipe: HttpServiceNotfoundPipe =
+      new HttpServiceNotfoundPipe();
+    expect(httpServiceNotfoundPipe.transform(value)).toStrictEqual(value);
   });
 });
